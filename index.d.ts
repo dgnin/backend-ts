3f4491b0acbd67514f97634e2ffde0a763d51c4b
dgnin <davidgomeznin@protonmail.com>
2024-11-06 09:27:09 +0100

    Adds env definitions

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,17 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       EXAMPLE_APP_PORT: string;
+
+      PRODUCTS_DB_HOST: string;
+      PRODUCTS_DB_USER: string;
+      PRODUCTS_DB_PASSWORD: string;
+      PRODUCTS_DB_DATABASE: string;
+      PRODUCTS_DB_PORT: string;
+      PRODUCTS_TEST_DB_HOST: string;
+      PRODUCTS_TEST_DB_USER: string;
+      PRODUCTS_TEST_DB_PASSWORD: string;
+      PRODUCTS_TEST_DB_DATABASE: string;
+      PRODUCTS_TEST_DB_PORT: string;
     }
   }
 }

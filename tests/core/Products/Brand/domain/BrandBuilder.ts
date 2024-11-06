7b6f5880dea91954e0947b62a3b260e8e85535e6
dgnin <davidgomeznin@protonmail.com>
2024-11-06 10:58:36 +0100

    Creates Product.buildInTestDB and Brand.buildInTestDB

--- tests/core/Products/Brand/domain/BrandBuilder.ts
+++ tests/core/Products/Brand/domain/BrandBuilder.ts
@@ -1,8 +1,13 @@
 import Brand from '@core/Products/Brand/domain/Brand';
 import { faker } from '@faker-js/faker/locale/en';
+import BrandId from '@core/Products/Brand/domain/BrandId';
 
 export default class BrandBuilder {
   public static buildRandom(): Brand {
     return new Brand(faker.word.noun());
+  }
+
+  public static buildInTestDB(): Brand {
+    return new Brand('Brand 1', new BrandId('aca8cca3-15e7-4f87-90ef-13879b33ab9e'));
   }
 }
--- tests/core/Products/Product/domain/ProductBuilder.ts
+++ tests/core/Products/Product/domain/ProductBuilder.ts
@@ -1,9 +1,18 @@
 import Product from '@core/Products/Product/domain/Product';
 import BrandBuilder from '../../Brand/domain/BrandBuilder';
 import { faker } from '@faker-js/faker/locale/en';
+import ProductId from '@core/Products/Product/domain/ProductId';
 
 export default class ProductBuilder {
   public static buildRandom(): Product {
     return new Product(faker.commerce.product(), BrandBuilder.buildRandom());
+  }
+
+  public static buildInTestDB(): Product {
+    return new Product(
+      'Product 1',
+      BrandBuilder.buildInTestDB(),
+      new ProductId('666d53cc-76f4-410c-9777-f7adc6bd14c8'),
+    );
   }
 }

2f43e8d114e1ffe93df890a1193bf223fc5447df
dgnin <davidgomeznin@protonmail.com>
2024-11-06 10:58:59 +0100

    Update GetProductController.test.ts

--- tests/apps/Example/controllers/Product/GetProductController.test.ts
+++ tests/apps/Example/controllers/Product/GetProductController.test.ts
@@ -2,6 +2,7 @@ import ProductId from '@core/Products/Product/domain/ProductId';
 import supertest from 'supertest';
 import app from '../../app';
 import httpStatus from 'http-status';
+import ProductBuilder from '../../../../core/Products/Product/domain/ProductBuilder';
 
 describe('GetProductController', () => {
   it("should return 404 if the Product doesn't exist", async () => {
@@ -11,5 +12,12 @@ describe('GetProductController', () => {
 
     expect(res.status).toEqual(httpStatus.NOT_FOUND);
     expect(res.body).toEqual(httpStatus[httpStatus.NOT_FOUND]);
+  });
+
+  it('should return the Product if it exists', async () => {
+    const product = ProductBuilder.buildInTestDB();
+    const res = await supertest(app).get(`/products/${product.id.value}`);
+
+    expect(res.status).toEqual(httpStatus.OK);
   });
 });
